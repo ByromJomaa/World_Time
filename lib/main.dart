94dbd1649caffbe280d5b916606aeb940db3fa5f
Byrom Jomaa <55561544+ByromJomaa@users.noreply.github.com>
2020-09-28 23:29:23 -0400

    Add Initial ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,14 @@ import 'package:world_time/pages/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.indigo[900],
+      accentColor: Colors.indigoAccent,
+      textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.grey[300]),
+          bodyText2: TextStyle(color: Colors.grey[900]),
+      ),
+    ),
     initialRoute: "/",
     routes: {
       "/": (context) => Loading(),
