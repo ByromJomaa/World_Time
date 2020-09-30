f36b31fb7bf783838c10fb6ebff0425de36daa8b
Byrom Jomaa <55561544+ByromJomaa@users.noreply.github.com>
2020-09-30 00:37:31 -0400

    Update choose_location.dart

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -44,28 +44,32 @@ class _ChooseLocationState extends State<ChooseLocation> {
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
+        toolbarHeight: 70,
         title: Text("Choose a Location"),
         centerTitle: true,
         elevation: 0,
       ),
-      body: ListView.builder(
-        itemCount: locations.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
-            child: Card(
-              child: ListTile(
-                onTap: () {
-                  updateTime(index);
-                },
-                title: Text(locations[index].location),
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage("assets/${locations[index].flag}"),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+        child: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    updateTime(index);
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage("assets/${locations[index].flag}"),
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
